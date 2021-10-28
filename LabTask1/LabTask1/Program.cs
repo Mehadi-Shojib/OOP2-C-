6cb2926059bf6f38bbb69e2c403def0f082cf4c1
Mehadi-Shojib <85815719+Mehadi-Shojib@users.noreply.github.com>
2021-10-29 00:20:32 +0600

    Add files via upload

---
+++ LabTask1/LabTask1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LaTask1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("MEHADI HASAN SHOJIB");//printing full name
+            Console.WriteLine("20-42713-1");//printing ID
+
+            //Implicit type casting between int and char
+            int word = 'S';
+            Console.WriteLine(word);
+            //Explicit type casting between int and double
+            double big =20.008;
+            int small = (int)big;//casted by casting operator 
+            Console.WriteLine(small);
+            Console.ReadKey();
+            
+
+        }
+    }
+}
